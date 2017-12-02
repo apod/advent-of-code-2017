a56bf15274ad5f2ec76ae48dba0d3129c5c1e12d
Alex Podaras <apodaras@gmail.com>
2017-12-02 10:42:29 +0200

    Add solution for first star of day 2

--- src/advent_of_code_2017/day02.clj
+++ src/advent_of_code_2017/day02.clj
@@ -1,11 +1,20 @@
-(ns advent-of-code-2017.day02)
+(ns advent-of-code-2017.day02
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn parse-input [s]
+  (let [rows (str/split-lines s)]
+    (map (fn [row-s]
+           (read-string (str \[ row-s \]))) rows)))
 
 (defn row-difference [row]
-  nil)
+  (reduce - (apply (juxt max min) row)))
 
-(defn checksum [input]
-  nil)
+(defn checksum [rows]
+  (reduce + (map row-difference rows)))
 
 (comment
+  (let [spreadsheet (parse-input (slurp (io/resource "day02/input.txt")))]
+    (checksum spreadsheet))
   )
 
--- test/advent_of_code_2017/day02_test.clj
+++ test/advent_of_code_2017/day02_test.clj
@@ -1,6 +1,13 @@
 (ns advent-of-code-2017.day02-test
-  (:require [clojure.test :refer :all]
-            [advent-of-code-2017.day02 :refer [row-difference checksum]]))
+  (:require [advent-of-code-2017.day02 :refer [checksum parse-input row-difference]]
+            [clojure.test :refer :all]))
+
+(deftest parse-input-test
+  (is (= (parse-input "5\t1\t9\t5") [[5 1 9 5]]))
+  (is (= (parse-input "5\t1\t9\t5\n7\t5\t3") [[5 1 9 5]
+                                              [7 5 3]]))
+  (is (= (parse-input "5 1 9 5\n7 5 3") [[5 1 9 5]
+                                         [7 5 3]])))
 
 (deftest row-difference-given-examples
   (is (= (row-difference [5 1 9 5]) 8))
