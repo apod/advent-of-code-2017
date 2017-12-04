54279513525436b0537f250afa3fa9f913b911c1
Alex Podaras <apodaras@gmail.com>
2017-12-04 22:49:54 +0200

    Add solution for first star of day 3

--- src/advent_of_code_2017/day03.clj
+++ src/advent_of_code_2017/day03.clj
@@ -1,9 +1,57 @@
 (ns advent-of-code-2017.day03)
 
-(defn steps [square]
-  nil)
+(def abs #(Math/abs %))
+
+(defn manhattan-distance [[x y]]
+  (+ (abs x) (abs y)))
+
+(defn spiral-walker
+  ([]
+   (spiral-walker [0 0] :right))
+  ([position direction]
+   {:position position
+    :direction direction}))
+
+(defn move [{:keys [position direction] :as walker}]
+  (->> (case direction
+         :right (update position 0 inc)
+         :up    (update position 1 inc)
+         :left  (update position 0 dec)
+         :down  (update position 1 dec))
+       (assoc walker :position)))
+
+(defn turn [{:keys [direction] :as walker}]
+  (->> (case direction
+         :right :up
+         :up    :left
+         :left  :down
+         :down  :right)
+       (assoc walker :direction)))
+
+(defn walk-to [walker from-n to-n]
+  (loop [{:keys [position] :as walker} walker
+         n from-n]
+    (if (= n to-n)
+      walker
+      (let [[x y] position
+            action (cond
+                     ;; Handle initial [0 0] position
+                     (= position [0 0]) move
+                     ;; Handle [1 0] position
+                     (= position [1 0]) (comp move turn)
+                     ;; Handle bottom right expansion to the right
+                     (and (neg? y) (= (- x) y)) move
+                     ;; Handle bottom right diagonal expansion turn
+                     (and (neg? y) (= (- x) (dec y))) (comp move turn)
+                     ;; Handle top left/right diagonal turns
+                     (= (abs x) (abs y)) (comp move turn)
+                     :else move)]
+        (recur (action walker) (inc n))))))
+
+(defn distance-from [n]
+  (manhattan-distance (:position (walk-to (spiral-walker) 1 n))))
 
 (comment
   (let [input 325489]
-    (steps input))
+    (distance-from input))
   )
--- test/advent_of_code_2017/day03_test.clj
+++ test/advent_of_code_2017/day03_test.clj
@@ -1,10 +1,42 @@
 (ns advent-of-code-2017.day03-test
-  (:require [advent-of-code-2017.day03 :refer [steps]]
+  (:require [advent-of-code-2017.day03 :refer [walk-to spiral-walker
+                                               move turn distance-from]]
             [clojure.test :refer :all]))
 
-(deftest steps-givel-examples
-  (is (= (steps 1) 0))
-  (is (= (steps 12) 3))
-  (is (= (steps 23) 2))
-  (is (= (steps 1024) 31)))
+(deftest move-test
+  (is (= (:position (move (spiral-walker [0 0] :right))) [ 1  0]))
+  (is (= (:position (move (spiral-walker [0 0] :up)))    [ 0  1]))
+  (is (= (:position (move (spiral-walker [0 0] :down)))  [ 0 -1]))
+  (is (= (:position (move (spiral-walker [0 0] :left)))  [-1  0])))
+
+(deftest turn-test
+  (is (= (:direction (turn (spiral-walker [0 0] :right))) :up))
+  (is (= (:direction (turn (spiral-walker [0 0] :up)))    :left))
+  (is (= (:direction (turn (spiral-walker [0 0] :down)))  :right))
+  (is (= (:direction (turn (spiral-walker [0 0] :left)))  :down)))
+
+(deftest move-and-turn-test
+  (is (= (-> (spiral-walker [0 0] :right)
+             move turn move turn move move turn)
+         {:position [-1 1] :direction :down}))
+  (is (= (-> (spiral-walker [-1 1] :down)
+             turn move move turn turn turn move turn turn turn move turn turn)
+         {:position [0 0] :direction :right})))
+
+(deftest walk-to-test
+  (is (= (:position (walk-to (spiral-walker) 1 1)) [0 0]))
+  (is (= (:position (walk-to (spiral-walker) 1 3)) [1 1]))
+  (is (= (:position (walk-to (spiral-walker) 1 5)) [-1 1]))
+  (is (= (:position (walk-to (spiral-walker) 1 7)) [-1 -1]))
+  (is (= (:position (walk-to (spiral-walker) 1 8)) [0 -1]))
+  (is (= (:position (walk-to (spiral-walker) 1 9)) [1 -1]))
+  (is (= (:position (walk-to (spiral-walker) 1 10)) [2 -1]))
+  (is (= (:position (walk-to (spiral-walker) 1 13)) [2 2]))
+  (is (= (:position (walk-to (spiral-walker) 1 18)) [-2 1])))
+
+(deftest distance-from-given-examples
+  (is (= (distance-from 1) 0))
+  (is (= (distance-from 12) 3))
+  (is (= (distance-from 23) 2))
+  (is (= (distance-from 1024) 31)))
 
