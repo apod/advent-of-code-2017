c9bdc3ba45a0702a33ee6fedd5125b371059eb18
Alex Podaras <alex@kinems.com>
2017-12-04 07:47:28 +0200

    Add solution for first star of day 4

--- src/advent_of_code_2017/day04.clj
+++ src/advent_of_code_2017/day04.clj
@@ -1,7 +1,14 @@
-(ns advent-of-code-2017.day04)
+(ns advent-of-code-2017.day04
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
 (defn valid-passphrase? [passphrase]
-  nil)
+  (every? #(= (val %) 1)
+          (frequencies (str/split passphrase #"\s+"))))
 
 (comment
+  (let [input (str/split-lines (slurp (io/resource "day04/input.txt")))]
+    ;; First star
+    (count (filter true? (map valid-passphrase? input)))
+    )
   )
