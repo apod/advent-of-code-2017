29e9b653cb5cab779766518df1c67807342727a2
Alex Podaras <apodaras@gmail.com>
2017-12-06 02:14:42 +0200

    Refactor day 5 steps fn to use get instead of manual bound checking

--- src/advent_of_code_2017/day05.clj
+++ src/advent_of_code_2017/day05.clj
@@ -1,15 +1,12 @@
 (ns advent-of-code-2017.day05)
 
 (defn steps [coll update-fn]
-  (let [max-pos (count coll)]
-    (loop [pos 0 coll coll steps 0]
-      (if (or (neg? pos) (>= pos max-pos))
-        steps
-        (let [current (nth coll pos)
-              f (update-fn current)]
-          (recur (+ pos current)
-                 (update coll pos f)
-                 (inc steps)))))))
+  (loop [pos 0 coll coll steps 0]
+    (if-let [current (get coll pos)]
+      (recur (+ pos current)
+             (update coll pos (update-fn current))
+             (inc steps))
+      steps)))
 
 (comment
   (let [input (read-string (str \[ (slurp (clojure.java.io/resource "day05/input.txt")) \]))]
