256f39b0146a0c4af499e0a8572233d0caeda4f2
Alex Podaras <apodaras@gmail.com>
2017-12-09 19:06:44 +0200

    Update to clojure 1.9.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject advent-of-code-2017 "0.1.0-SNAPSHOT"
   :description "My solutions for AoC 2017"
   :url "http://adventofcode.com/"
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]])
