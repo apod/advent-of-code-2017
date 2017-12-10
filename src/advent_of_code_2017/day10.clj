e29716a38122e59a2c81e9e4a7485090e5e699b7
Alex Podaras <apodaras@gmail.com>
2017-12-11 00:17:21 +0200

    Add solution for second star of day 10

--- src/advent_of_code_2017/day10.clj
+++ src/advent_of_code_2017/day10.clj
@@ -1,4 +1,5 @@
-(ns advent-of-code-2017.day10)
+(ns advent-of-code-2017.day10
+  (:require [clojure.string :as str]))
 
 (defn reverse-portion [coll start length]
   (let [size (count coll)
@@ -9,18 +10,36 @@
     (reduce (fn [coll [pos e]]
               (assoc coll (mod pos size) e)) coll reverse-positions)))
 
-(defn knot-hash [range lengths]
+(defn knot-hash [range lengths rounds]
   (loop [coll (vec range)
-         [length & lengths] lengths
-         pos 0 skip 0]
-    (if (nil? length)
-      coll
-      (recur (reverse-portion coll pos length)
-             lengths (+ pos length skip) (inc skip)))))
+         [length & remaining] lengths
+         pos 0 skip 0
+         round rounds]
+    (cond
+      (zero? round) coll
+      (nil? length) (do
+                      (println (dec round))
+                      (recur coll lengths pos skip (dec round)))
+      :else (recur (reverse-portion coll pos length) remaining
+                   (+ pos length skip) (inc skip) round))))
+
+(defn sparse-hash [input]
+  (let [rounds 64
+        suffix [17 31 73 47 23]
+        ascii (into (mapv int (str/trim input)) suffix)]
+    (knot-hash (range 256) ascii rounds)))
+
+(defn dense-hash [hash]
+  (map #(reduce bit-xor %) (partition 16 hash)))
+
+(defn hex-string [hash]
+  (str/join (map #(format "%02x" %) hash)))
 
 (comment
   (let [input (slurp (clojure.java.io/resource "day10/input.txt"))
         lengths (read-string (str \[ input \]))]
     ;; First star
-    (reduce * (take 2 (knot-hash (range 256) lengths))))
+    (reduce * (take 2 (knot-hash (range 256) lengths 1)))
+    ;; Second star
+    (hex-string (dense-hash (sparse-hash input))))
   )
--- test/advent_of_code_2017/day10_test.clj
+++ test/advent_of_code_2017/day10_test.clj
@@ -10,8 +10,8 @@
     (is (= (reverse-portion [4 3 0 1 2] 1 5) [3 4 2 1 0]))))
 
 (deftest knot-hash-given-examples
-  (is (= (knot-hash (range 5) [3]) [2 1 0 3 4]))
-  (is (= (knot-hash (range 5) [3 4]) [4 3 0 1 2]))
-  (is (= (knot-hash (range 5) [3 4 1]) [4 3 0 1 2]))
-  (is (= (knot-hash (range 5) [3 4 1 5]) [3 4 2 1 0]))
+  (is (= (knot-hash (range 5) [3] 1) [2 1 0 3 4]))
+  (is (= (knot-hash (range 5) [3 4] 1) [4 3 0 1 2]))
+  (is (= (knot-hash (range 5) [3 4 1] 1) [4 3 0 1 2]))
+  (is (= (knot-hash (range 5) [3 4 1 5] 1) [3 4 2 1 0]))
   )
